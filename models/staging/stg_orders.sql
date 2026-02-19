c658793b5b5360b594c004bc4ea0b837eab5b278
dara bindra <bindara2002@gmail.com>
2026-02-19 16:56:53 +0000

    commit

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -1,7 +1,26 @@
+with ranked_orders as (
+
+    select
+        order_id,
+        customer_id,
+        to_date(order_date) as order_date,
+        status,
+        cast(total_amount as number(10,2)) as total_amount,
+        row_number() over (
+            partition by order_id
+            order by order_date desc
+        ) as rn
+
+    from {{ source('bronze', 'orders_raw') }}
+
+)
+
 select
     order_id,
     customer_id,
-    to_date(order_date) as order_date,
+    order_date,
     status,
-    cast(total_amount as number(10,2)) as total_amount
-from {{ source('bronze', 'orders_raw') }}
+    total_amount
+from ranked_orders
+where rn = 1
+
